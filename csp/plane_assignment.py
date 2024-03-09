632829b7422fc27375e2678f3db1be166d58fbc3
jvl4422 <91969088+jvl4422@users.noreply.github.com>
2024-03-08 20:41:01 -0500

    Added documentation

--- csp/plane_assignment.py
+++ csp/plane_assignment.py
@@ -5,20 +5,26 @@ to the different routes that they offer, over a day.
 Authors: Karina Verma and Jasmine van Leeuwen
 
 GETTING STARTED:
+We recommend you make a new Python environment for this project to avoid any potential library clashes.
 If you made a new conda env, make sure to navigate to that env and activate it in terminal
 (this can be done through the anaconda interface).
 Once your environment has been activated (you'll know because it will have the name
 surrounded in parentheses), execute the following in terminal: python -m pip install --upgrade --user ortools
-Then hopefully colouring will show up, and you'll be able to verify it has been installed correctly.
+Then hopefully colouring will show up here which verifies it has been installed correctly.
 """
-
-
 from ortools.sat.python import cp_model
 import pandas as pd
 
+"""
+This function creates and formats the CP Model by reading in the data and adding the relevant constraints.
+
+:returns model - CP Model, planes, idx_to_serve, varbs, a330200
+         planes - list of all plane names
+         idx_to_serve - dictionary with indices which correspond to the rows that are the destinations to be served
+         varbs - the variables added to the CP Model
+         a330200 - the fit score data frame
+"""
 def format_model():
-    # Should add some error checking here to say that if there are more dests to serve than planes, it is impossible to
-    # solve this model
     model = cp_model.CpModel()
     dests_to_serve = open("satisfy_destinations.txt", "r").read().split()
     a330200 = pd.read_csv("a330200.csv", skiprows=1)
@@ -28,8 +34,7 @@ def format_model():
         exit(1)
     max_total_fit = 100
 
-    # I think I can give it intuitive names
-    # Gonna make this a set and see, just because I don't forsee myself needing it again.
+    # Made a list of all destinations
     all_dests = []
     idx_to_serve = {}
     # Add all of our destinations to a list in case we need it later
@@ -37,21 +42,22 @@ def format_model():
         all_dests.append(dest)
     # Find the indices of the destinations we need to serve. This will be useful in making sure that we only
     # check the data that we need instead of everything.
+
+    # Find the row index of each destination that is to be served.
     for each in dests_to_serve:
         idx = all_dests.index(each)
         idx_to_serve[idx] = each
 
     # Now that we have isolated our data, we are ready to create our variables.
     varbs = {}
-    rows_of_interest = idx_to_serve.keys()
+    # Create a boolean variable for each plane and destination combination.
+
     for eachplane in planes:
         for eachdest in dests_to_serve:
             # I am still not entirely sure what the fstring is for but anyway!
             varbs[eachplane, eachdest] = model.NewBoolVar(f"varbs[{eachplane},{eachdest}]")
 
     # Now we add our constraints
-    # The variables that are true for one destination must all be different
-    # Meaning one destination can only have one plane
 
     # This constraint makes sure that one plane is not assigned to multiple destinations
     tack = []
@@ -63,22 +69,14 @@ def format_model():
         # print(diffList, "must cannot all be true")
 
     # TODO: Strange bug but this is the only way to get it to work and have unique planes flying to unique destinations
+    # I need to figure out how to resolve this lol bc hardcoding is NOT the answer
     i = 0
     while i < 8:
         model.AddExactlyOne(tack[i])
-        print(tack[i])
         i += 1
 
-    # model.AddExactlyOne(testing[0])
-    # model.AddExactlyOne(testing[1])
 
-
-    # Each destination is added to exactly one plane
-    # destination = task
-    # plane = worker
-    # This constraint is supposed to make sure that one destination is not assigned to multiple planes
-    # but it is broken and I do not know why
-
+    # Ensure that one destination is not served by multiple planes.
     for eachdest in dests_to_serve:
         diffList = []
         for eachplane in planes:
@@ -90,17 +88,22 @@ def format_model():
     objective_terms = []
     for eachplane in planes:
         for eachdestidx in idx_to_serve.keys():
-            #if a330200[eachplane][eachdestidx] == 75:
-                # print("Found an optimal match for", eachplane, "to", idx_to_serve[eachdestidx])
             objective_terms.append(a330200[eachplane][eachdestidx] * varbs[eachplane, idx_to_serve[eachdestidx]])
     model.Maximize(sum(objective_terms))
     return model, planes, idx_to_serve, varbs, a330200
 
+"""
+This method solves the constraint satisfaction problem and prints the results to the console. 
+
+
+"""
 def solve_results(model, planes, dests, varbs, fitscores):
     solver = cp_model.CpSolver()
     status = solver.Solve(model)
     if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
         print("Successful assignment!")
+        if status == cp_model.OPTIMAL:
+            print("Optimal Solution")
         print(f"Fit score = {solver.ObjectiveValue()}\n out of", len(planes)*100)
         print("Assignments are as follows: ")
         for plane in planes:
